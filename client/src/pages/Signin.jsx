097d2c4104ab0f618139df6c7a0c30236532fbae
Muthukumar1911 <amamuthukumar@gmail.com>
2024-03-16 16:24:47 +0530

    signin completed

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+import axios from 'axios'
+
 
 export default function Signin() {
+  const navigate = useNavigate()
+  const [formData, setFormData] = useState({})
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id] : e.target.value})
+  }
+
+  const handleSubmit = async (e) => {
+    setLoading(true)
+    e.preventDefault()
+    const {password, email} = formData
+    console.log(formData)
+    await axios.post('http://127.0.0.1:3000/api/auth/signin', {
+      password,
+      email
+    })
+    .then((res) => {
+      console.log(res)
+      setLoading(false)
+      setError(false)
+      navigate('/')
+    })
+    .catch((err) => {
+      console.log(err)
+      setLoading(false)
+      setError(true)
+    })
+  }
+  
+
   return (
-    <div>Signin</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+    
+    <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+     
+      <input type='email' placeholder='Email' id='email' 
+      className='bg-slate-100 p-3' onChange={handleChange}/>
+      
+      <input type='text' placeholder='Password' id='password' 
+      className='bg-slate-100 p-3' onChange={handleChange}/>
+
+      <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading..' : 'Sign Up'}</button>
+    
+    <div className='flex gap-2 mt-5'>
+      <p>Dont have an account?</p>
+      <Link to='/signup'>
+      <span className='text-blue-500'>Sign Up</span></Link>
+    </div>
+    </form>
+    <p className='text-red-600 font-bold'>{error ? 'Something went wrong !' : ''}</p>
+    </div>
   )
 }
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import axios from 'axios'
 
 
 export default function Signup() {
+  const navigate = useNavigate()
   const [formData, setFormData] = useState({})
   const [error, setError] = useState(null)
   const [loading, setLoading] = useState(false)
@@ -25,6 +26,7 @@ export default function Signup() {
       console.log(res)
       setLoading(false)
       setError(false)
+      navigate('/')
     })
     .catch((err) => {
       console.log(err)
