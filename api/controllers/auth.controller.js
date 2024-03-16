27b053c82cd76ed7e6969ffb6a6b5cf13f212f9d
Muthukumar1911 <amamuthukumar@gmail.com>
2024-03-16 16:14:10 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res) => {
     const {username, password, email} = req.body;
@@ -18,3 +19,25 @@ export const signup = async (req, res) => {
         res.status(500).json(err);
     }
 };
+
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body
+    console.log(req.body)
+    try {
+        const validUser = await User.findOne({email})
+        if(!validUser){
+            return next(errorHandler(401, 'User not found'))
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if(!validPassword){
+            return next(errorHandler(401, 'Wrong credentials'))
+        }
+        const token = jwt.sign({id : validUser._id}, process.env.JWT_SECRET)
+        const {password : hashedPassword, ...rest} = validUser._doc
+        const expiryDate = new Date(Date.now() + 3600000)
+        res.cookie('access_token', token, {httpOnly : true, expires: expiryDate}).status(200).json(rest)
+    } catch (error) {
+        next(error)
+    }
+}
